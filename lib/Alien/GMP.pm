32ff3da5bc4493ed90c563393e6c30dd18649db6
Graham Ollis <plicease@cpan.org>
2017-12-06 11:34:25 -0500

    add support for libgmpxx

--- lib/Alien/GMP.pm
+++ lib/Alien/GMP.pm
@@ -2,10 +2,12 @@ package Alien::GMP;
 
 use strict;
 use warnings;
-
 use parent 'Alien::Base';
+use Role::Tiny::With qw( with );
 
 # VERSION
 # ABSTRACT: Alien package for the GNU Multiple Precision library.
+
+with 'Alien::Role::Alt';
 
 1;
